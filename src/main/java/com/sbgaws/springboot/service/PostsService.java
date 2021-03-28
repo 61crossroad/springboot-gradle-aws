d84ba2715011d814bff31b76c91a83f19bede9f3
61crossroad <61crossroad@gmail.com>
2021-03-29 01:01:14 +0900

    JPA save() with RequestDto

---
+++ src/main/java/com/sbgaws/springboot/service/PostsService.java
@@ -0,0 +1,18 @@
+package com.sbgaws.springboot.service;
+
+import com.sbgaws.springboot.domain.posts.PostsRepository;
+import com.sbgaws.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
